bc7bab950aa759a6beef04b1411d5f80c7fbc819
rakvat <rakvat@users.noreply.github.com>
2015-10-11 14:50:18 -0700

    use tcpsocket double in test

--- spec/send_command_spec.rb
+++ spec/send_command_spec.rb
@@ -3,7 +3,11 @@ require "lib/send"
 
 describe "send" do
   it "writes to socket" do
-    expect_any_instance_of(TCPSocket).to receive(:write)
+    tcpsocket = double(TCPSocket)
+    allow(TCPSocket).to receive(:new) { tcpsocket }
+    allow(tcpsocket).to receive(:write)
+    allow(tcpsocket).to receive(:close)
+    expect(tcpsocket).to receive(:write)
     Send.perform([])
   end
 end
